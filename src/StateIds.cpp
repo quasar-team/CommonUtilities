4c172041c6dafc2cd1ff7845f5c65939b0f71169
Ben Farnham <benjamin.farnham@cern.ch>
2018-09-12 08:15:37 +0200

    added LGPL headers

--- src/StateIds.cpp
+++ src/StateIds.cpp
@@ -1,3 +1,23 @@
+/* © Copyright CERN, 2018. All rights not expressly granted are reserved. */
+/*
+ *
+ *  Created on: Sep 12, 2018
+ *      Author: Ben Farnham <ben.farnham@cern.ch>
+ *
+ *  This file is part of Quasar.
+ *
+ *  Quasar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public Licence as published by
+ *  the Free Software Foundation, either version 3 of the Licence.
+ *
+ *  Quasar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Quasar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "StateIds.h"
 
 const std::string STATE_ID_NAMES[] = {"CONNECT", "SYSQUERY", "COMMANDS", "DISCONNECT", "END"};
